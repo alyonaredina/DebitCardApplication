b46cc42e5d3c3b72305d5771d044ada8d124159d
Alyona Redina <medvedeva_64@mail.ru>
2024-06-03 02:23:43 +0600

    first test

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -11,12 +11,12 @@ class CallbackTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        SelenideElement form = $ (".form");
+        form.$("[data-test-id=name] input").setValue("Алена Редина");
+        form.$("[data-test-id=phone] input").setValue("+79277970093");
         form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        form.$(".button__content").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
 
